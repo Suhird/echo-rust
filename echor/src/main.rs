ede0d55777f323ecd2a48e64a52bc916f49d042a
Suhird <suhirdsingh92@gmail.com>
2023-06-24 16:37:56 -0400

    updated with 2 more args

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,9 +1,23 @@
-use clap::App;
+use clap::{App, Arg};
 
 fn main() {
-       let _match = App::new("echor")
+       let matches = App::new("echor")
         .version("0.1.1")
         .author("Suhird singh <suhirdsingh92@gmail.com>")
         .about("echo in Rust")
+           .arg(
+                  Arg::with_name("text")
+                      .value_name("TEXT")
+                      .help("Input text")
+                      .required(true)
+                      .min_values(1),
+           )
+           .arg(
+               Arg::with_name("omit_newline")
+                   .short("n")
+                   .help("Do not print newline")
+                   .takes_value(false)
+           )
         .get_matches();
+    println!("{:#?}", matches);
 }
